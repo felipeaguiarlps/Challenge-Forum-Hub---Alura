a1aab33941c05614db7f37d7713c744e4588b835
Felipe Aguiar Lopes <felipe.aguiar96@hotmail.com>
2024-07-16 22:06:22 -0300

    Projeto Spring Boot initialized

---
+++ src/main/java/com/br/forumhubalura/ForumHubAluraApplication.java
@@ -0,0 +1,13 @@
+package com.br.forumhubalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubAluraApplication.class, args);
+	}
+
+}
